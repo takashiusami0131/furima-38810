a9ff8cda8a82eda2df2fc8059de37024ccd7eb0a
takashiusami0131 <monkey_t0131@yahoo.co.jp>
2023-01-25 14:05:19 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nick_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name,       presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana,  presence: true
-  validates :birth_day,       presence: true
+  validates :nickname,         presence: true
+  validates :first_name,       presence: true
+  validates :family_name,      presence: true
+  validates :first_name_kana,  presence: true
+  validates :family_name_kana, presence: true
+  validates :birth_day,        presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
-    validates :last_name
+    validates :family_name
   end
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
     validates :first_name_kana
-    validates :last_name_kana
+    validates :family_name_kana
   end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
