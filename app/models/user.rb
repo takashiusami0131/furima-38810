2c07af228a2db5082628a03c136b5e518c0e33db
takashiusami0131 <monkey_t0131@yahoo.co.jp>
2023-01-25 18:39:38 +0900

    モデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,         presence: true
-  validates :first_name,       presence: true
-  validates :family_name,      presence: true
-  validates :first_name_kana,  presence: true
-  validates :family_name_kana, presence: true
+  validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :family_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' }
   validates :birth_day,        presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name
-    validates :family_name
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
-    validates :first_name_kana
-    validates :family_name_kana
-  end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
