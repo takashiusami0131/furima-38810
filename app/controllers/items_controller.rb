97f05277fd5d97aeba375fae8ca945e59e05383d
takashiusami0131 <monkey_t0131@yahoo.co.jp>
2023-01-26 16:34:01 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+  end
+
+  def new
   end
 
   private
 
-  def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
-  end
+  # def item_params
+  #   params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :, :shipping_cost_id, :province_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  # end
 end
