2b4be81e319f7aa4096d51fed0ccc87d802a83dd
takashiusami0131 <monkey_t0131@yahoo.co.jp>
2023-01-26 15:33:39 +0900

    ActiveHash導入・バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :province
+  belongs_to :sipping_day
+
   belongs_to :user
   has_one_attached :image
+
+  validates :image,           presence: true
+  validates :title,           presence: true, length: { maximum: 40 }
+  validates :text,            presence: true, length: { maximum: 1000 }
+  validates :category_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :sipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :province_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :sipping_day_id,  presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,           presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 end
