0246cf2bc7a782fe96115886b704fd3e447f97c4
takashiusami0131 <monkey_t0131@yahoo.co.jp>
2023-01-27 14:30:26 +0900

    販売手数料計算処理

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image,           presence: true
-  validates :title,           presence: true, length: { maximum: 40 }
-  validates :text,            presence: true, length: { maximum: 1000 }
-  validates :category_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :status_id,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :sipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :province_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :sipping_day_id,  presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price,           presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+  validates :image,            presence: true
+  validates :name,             presence: true, length: { maximum: 40 }
+  validates :description,      presence: true, length: { maximum: 1000 }
+  validates :category_id,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id,        presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :province_id,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id,  presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,            presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 end
+
