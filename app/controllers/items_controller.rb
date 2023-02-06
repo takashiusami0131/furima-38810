640f27fb887b75cd87e46188ee1e4e6185e01ce5
takashiusami0131 <monkey_t0131@yahoo.co.jp>
2023-02-06 21:53:12 +0900

    sold out 設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if @item.purchase.present? || current_user != @item.user
+      redirect_to root_path 
+    end
   end
 
   def update
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
   before_action :set_item, only: [:index, :create]
  
   def index
